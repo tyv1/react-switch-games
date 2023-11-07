6b7ac00575b7281fa976ff6f8be65e850f901b5e
Emil <eetaller@gmail.com>
2023-11-08 00:36:06 +0100

    Remove unused import and move show more button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Divider } from '@mui/material';
 import './App.css';
 import GamesGrid from './components/GamesGrid';
 import { useState } from 'react';
@@ -14,12 +13,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Switch Games</h1>
-        <button onClick={toggleShowMore}>
-          {showMore ? 'Show Less' : 'Show More'}
-        </button>
       </header>
       <body className="App-body">
         <GamesGrid showMore={showMore} />
+        <button onClick={toggleShowMore}>
+          {showMore ? 'Show Less' : 'Show More'}
+        </button>
       </body>
     </div>
   );
